9c9f3f44c1d26450f93754b44f0177c048392355
sheksha7869 <102206726+sheksha7869@users.noreply.github.com>
2023-01-30 12:34:34 +0530

    New project
    New project

---
+++ index.js
@@ -0,0 +1,31 @@
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+    if (req.url == '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is the Default Page of the Server. If You have not specified anything or a Address, this page should be visible.</p></body></html>');
+        res.end();
+
+    }
+    else if (req.url == "/home") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is Home Page. Hello All And Good Morning.</p></body></html>');
+        res.end();
+
+    }
+    else if (req.url == "/dashboard") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is Dashboard Page. Hello All And Good Morning.</p></body></html>');
+        res.end();
+    }
+    else if (req.url == "/error") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is Error Page. Hello All And Good Morning.</p></body></html>');
+        res.end();
+    }
+    else
+        res.end('Invalid Request!');
+});
+
+server.listen(5000);
+console.log('Node.js web server at port 5000 is running..')
